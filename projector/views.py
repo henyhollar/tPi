75832391cfc35b4742701f3358a3d251b880dd56
Heny Hollar <henyhollar@gmail.com>
2016-05-01 22:33:33 +0200

    convert to slide

--- projector/views.py
+++ projector/views.py
@@ -13,7 +13,7 @@ def convert_to_slide(request, **kwargs):
     path_base_dir = Path(base_dir)
     path_to_rst_file = path_base_dir.joinpath('media/{}/{}'.format(kwargs.get('course_code'), kwargs.get('file_name')))
     if path_to_rst_file.exists() and Path(kwargs.get('file_name')).ext == '.rst':
-        os.system("landslide {} -i -d {}/media/{}/{}.html".format(path_to_rst_file, base_dir, kwargs.get('course_code'), kwargs.get('file_name')))
+        os.system("landslide {} -i -d {}/media/{}/{}.html".format(path_to_rst_file, base_dir, kwargs.get('course_code'), kwargs.get('file_name').split('.')[0]))
     else:
         return HttpResponse('HTML slide not created. Please make sure the file exists or has extension .rst')
 
